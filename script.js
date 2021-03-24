f019d712830a111470f52d5ccbf36c3aaa8d5eaf
alexia-avelar <a.avelar8d2015@gmail.com>
2021-03-23 21:11:50 -0600

    vfinal

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function imgSlider(drink){
+    document.querySelector(".pepsi").src = drink;
 }
 
 function changeBgColor(color){
+    const sec = document.querySelector(".sec");
+    sec.style.background = color;
 }
